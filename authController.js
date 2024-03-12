b169a0fa9a999527c77c5cf0f4a0afad42d94361
Thomasnator1401 <145989955+Thomasnator1401@users.noreply.github.com>
2024-03-12 16:32:26 -0400

    Update authController.js

--- authController.js
+++ authController.js
@@ -1,90 +1,89 @@
-const User = require("../models/User");
-const jwt = require('jsonwebtoken');
+Const consumer = require("../fashions/person");
+Const jwt = require('jsonwebtoken');
 
-// handle errors
-const handleErrors = (err) => {
-  console.log(err.message, err.code);
-  let errors = { email: '', password: '' };
+// manage errors
+Const handleErrors = (err) => 
+  console.Log(err.Message, err.Code);
+  allow mistakes =  e-mail: '', password: '' ;
 
   // incorrect email
-  if (err.message === 'incorrect email') {
-    errors.email = 'That email is not registered';
-  }
+  if (err.Message === 'wrong e mail') 
+    errors.Electronic mail = 'That e mail isn't always registered';
+  
 
-  // incorrect password
-  if (err.message === 'incorrect password') {
-    errors.password = 'That password is incorrect';
-  }
+  // wrong password
+  if (err.Message === 'incorrect password') 
+    mistakes.Password = 'That password is wrong';
+  
 
-  // duplicate email error
-  if (err.code === 11000) {
-    errors.email = 'that email is already registered';
-    return errors;
-  }
+  // replica electronic mail errors
+  if (err.Code === 11000) 
+    mistakes.E mail = 'that e mail is already registered';
+    go back errors;
+  
 
-  // validation errors
-  if (err.message.includes('user validation failed')) {
-    // console.log(err);
-    Object.values(err.errors).forEach(({ properties }) => {
-      // console.log(val);
-      // console.log(properties);
-      errors[properties.path] = properties.message;
-    });
-  }
+  // validation mistakes
+  if (err.Message.Consists of('person validation failed')) 
+    // console.Log(err);
+    item.Values(err.Mistakes).ForEach(( properties ) => 
+      // console.Log(val);
+      // console.Log(residences);
+      mistakes[properties.Path] = properties.Message;
+    );
+  
 
-  return errors;
-}
+  go back errors;
 
-// create json web token
-const maxAge = 3 * 24 * 60 * 60;
-const createToken = (id) => {
-  return jwt.sign({ id }, 'net ninja secret', {
+
+// create json internet token
+Const maxAge = 3 * 24 * 60 * 60;
+Const createToken = (identity) => 
+  return jwt.Signal( id , 'net ninja secret', 
     expiresIn: maxAge
-  });
-};
+  );
+;
 
-// controller actions
-module.exports.signup_get = (req, res) => {
-  res.render('signup');
-}
+// controller movements
+Module.Exports.Signup_get = (req, res) => 
+  res.Render('signup');
 
-module.exports.login_get = (req, res) => {
-  res.render('login');
-}
 
-module.exports.signup_post = async (req, res) => {
-  const { email, password } = req.body;
+Module.Exports.Login_get = (req, res) => 
+  res.Render('login');
 
-  try {
-    const user = await User.create({ email, password });
+
+Module.Exports.Signup_post = async (req, res) => 
+  const  e mail, password  = req.Frame;
+
+  try 
+    const person = anticipate user.Create( e-mail, password );
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(201).json({ user: user._id });
-  }
-  catch(err) {
+    res.Cookie('jwt', token,  httpOnly: true, maxAge: maxAge * one thousand );
+    res.Popularity(201).Json( user: consumer._id );
+  
+  catch(err) 
     const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
+    res.Popularity(400).Json( errors );
+  
  
-}
 
-module.exports.login_post = async (req, res) => {
-  const { email, password } = req.body;
 
-  try {
-    const user = await User.login(email, password);
-    const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
-    res.status(200).json({ user: user._id });
-  } 
-  catch (err) {
-    const errors = handleErrors(err);
-    res.status(400).json({ errors });
-  }
+Module.Exports.Login_post = async (req, res) => 
+  const  e mail, password  = req.Body;
 
-}
+  strive 
+    const user = await user.Login(e mail, password);
+    const token = createToken(person._id);
+    res.Cookie('jwt', token,  httpOnly: proper, maxAge: maxAge * one thousand );
+    res.Popularity(2 hundred).Json( user: consumer._id );
+   
+  capture (err) 
+    const mistakes = handleErrors(err);
+    res.Fame(four hundred).Json( errors );
+  
 
-module.exports.logout_get = (req, res) => {
-  res.cookie('jwt', '', { maxAge: 1 });
-  res.redirect('/');
-}
+
+
+Module.Exports.Logout_get = (req, res) => 
+  res.Cookie('jwt', '',  maxAge: 1 );
+  res.Redirect('/');
